4f59b5c616e812fd71e077629df2e1a0c00a214b
dapuck <imccall@da-puck.com>
2013-06-30 21:08:46 -0500

    Started on keyboard support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var mouse = require('node_mouse_mac');
+//var keyboard = require('node_keyboard_mac');
 var httpServer = require('http').createServer(httpHandler);
 var url = require('url');
 var io = require('socket.io').listen(httpServer);
 var fs = require('fs');
+//var mime = require('mime');
 
 httpServer.listen(8888);
 
@@ -10,7 +12,7 @@ io.sockets.on('connection',function(socket) {
 	socket.on('mouseMove', function(data) {
 		//console.log(data);
 		mouse.moveDelta(data.dx, data.dy);
-		mouse.show();
+		//mouse.show();
 	});
 	
 	socket.on('mouseClick', function(data) {
@@ -18,6 +20,17 @@ io.sockets.on('connection',function(socket) {
 		mouse.buttonDown();
 		setTimeout(mouse.buttonUp,50);
 		//mouse.buttonUp();
+	});
+	
+	// need to add code to handle modifiers
+	// (shift, ctl, alt, cmd)
+	socket.on('keyPress', function(data) {
+		console.log("key!");
+		var code = Math.round(data.keyCode);
+		if(isNaN(code))
+			return;
+		keyboard.keyDown(code);
+		setTimeout(keyboard.keyUp, 50, code);
 	});
 });
 
@@ -34,6 +47,7 @@ function httpHandler(req, res) {
 				res.writeHead(500);
 				return res.end("Critical Error!");
 			}
+			//res.setHeader("Content-Type", mime.lookup(parsedUrl.pathname));
 			res.writeHead(200);
 			res.end(data);
 		});
